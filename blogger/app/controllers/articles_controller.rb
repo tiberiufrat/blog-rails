296e8f26a2166d95edcf203ac99b5f43946b6bea
tiberiufrat <tiberiu.fratila@gmail.com>
2020-07-13 13:09:33 +0300

    add create and destroy flashes

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -16,12 +16,16 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' deleted!"
 
     redirect_to articles_path
   end
