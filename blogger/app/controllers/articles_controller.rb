64c9d56213f5e11a6ba683ba30f5b278b2eed4a3
tiberiufrat <tiberiu.fratila@gmail.com>
2020-07-13 13:05:56 +0300

    add flash for update method

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -34,6 +34,8 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
+    flash.notice = "Article '#{@article.title}' updated!"
+
     redirect_to article_path(@article)
   end
 end
